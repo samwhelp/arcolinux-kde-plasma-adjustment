047af69b253c1b59e4061464f4273c980e984937
samwhelp <samwhelp@users.noreply.github.com>
2024-04-26 11:24:14 +0800

    fix

---
+++ prototype/main/grub-config/grub-theme/grub-theme-darkmatter-remix/helper/ext/setting/module/init.sh
@@ -0,0 +1,17 @@
+
+
+################################################################################
+### Head: Setting / Module / Init
+##
+
+
+##
+## Load: Setting / Module / Grub-Theme
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/grub-theme.sh"
+
+
+##
+### Tail: Setting / Module / Init
+################################################################################
